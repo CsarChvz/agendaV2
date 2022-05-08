9fb24bbfc7544936d3ef8647a9af5a1d598dde95
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-05-08 03:49:12 +0000

    setEnbel en metodo de focus, para no poder agregar

--- src/main/java/chavez/cesar/agendav2/Captura.java
+++ src/main/java/chavez/cesar/agendav2/Captura.java
@@ -178,18 +178,15 @@ public class Captura extends javax.swing.JInternalFrame {
                 .addContainerGap()
                 .addGroup(JP_Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(JP_Panel1Layout.createSequentialGroup()
-                        .addGroup(JP_Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(JP_Panel1Layout.createSequentialGroup()
-                                .addComponent(JL_Sexo)
-                                .addGap(18, 18, 18)
-                                .addComponent(JR_Femenino)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(JR_Masculino))
-                            .addGroup(JP_Panel1Layout.createSequentialGroup()
-                                .addComponent(JL_Edad)
-                                .addGap(18, 18, 18)
-                                .addComponent(JT_Edad, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(JL_Sexo)
+                        .addGap(18, 18, 18)
+                        .addComponent(JR_Femenino)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(JR_Masculino))
+                    .addGroup(JP_Panel1Layout.createSequentialGroup()
+                        .addComponent(JL_Edad)
+                        .addGap(18, 18, 18)
+                        .addComponent(JT_Edad, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(JP_Panel1Layout.createSequentialGroup()
                         .addGroup(JP_Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, JP_Panel1Layout.createSequentialGroup()
@@ -224,11 +221,11 @@ public class Captura extends javax.swing.JInternalFrame {
                                             .addComponent(JT_ID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(JL_ID2)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
                         .addGroup(JP_Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING))
-                        .addGap(57, 57, 57))))
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel4))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         JP_Panel1Layout.setVerticalGroup(
             JP_Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -417,11 +414,12 @@ public class Captura extends javax.swing.JInternalFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(60, 60, 60)
-                        .addComponent(JP_Panel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(JP_Panel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 155, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap(38, Short.MAX_VALUE)
-                        .addComponent(JP_Panel1, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addContainerGap()
+                        .addComponent(JP_Panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(JP_Panel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
@@ -831,9 +829,14 @@ public class Captura extends javax.swing.JInternalFrame {
             Matcher matcher = pattern.matcher(JT_Email.getText());
             boolean matchFound = matcher.find();
             if (matchFound) {
+                // En esta parte podemos poner un icono con una palomita
                 jLabel5.setText("Match found");
+                JB_Agregar.setEnabled(true);
+                jLabel5.updateUI();
             } else {
-                System.out.println("Match not found");
+                jLabel5.setText("Match no found");
+                jLabel5.updateUI();
+                JB_Agregar.setEnabled(false);
             }
         }
     }//GEN-LAST:event_JT_EmailFocusLost
